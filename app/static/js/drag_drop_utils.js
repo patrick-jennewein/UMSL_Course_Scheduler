5bc5a86fa5fcb3c67f9772cb7995ba7ced88b080
Jordan Moore <jmoore@marathonts.com>
2024-04-19 11:33:50 -0500

    Removed use of alert message when a course fails validation on drag-and-drop. Courses will always be allowed to move, but will get a red border if the course fails validation on the move

--- app/static/js/drag_drop_utils.js
+++ app/static/js/drag_drop_utils.js
@@ -10,7 +10,6 @@ function drag(ev, course_element) {
     ev.dataTransfer.setData("course_num", course_num);
     ev.dataTransfer.setData("course_name", course_name);
     ev.dataTransfer.setData("course_credits", course_credits);
-    ev.dataTransfer.setData("course_element", course_element);
 }
 
 function allowDrop(ev) {
@@ -23,6 +22,8 @@ function dropFailedAlert (msg, li_element) {
 }
 
 function prereqVerification(course_info, course_num, semester_num, li_to_move, course_name, is_prereq_for_check = false, orig_course_num) {
+    let li_to_update = li_to_move;
+
     let alert_message = "";
     let should_move_course = true;
     let required_courses_taken = false;
@@ -54,6 +55,7 @@ function prereqVerification(course_info, course_num, semester_num, li_to_move, c
             credits_total_for_new_semester = credits_total_for_new_semester + course_schedule[i].credits;
         }
     } else {
+        li_to_update = document.getElementById(course_num)
         let semester_of_prereq_course = null;
         course_schedule.some((semester) => {
             semester_schedule = semester.schedule.map(x => x.course);
@@ -138,8 +140,9 @@ function prereqVerification(course_info, course_num, semester_num, li_to_move, c
         }
     })
     if (!required_courses_taken) {
-        dropFailedAlert(alert_message, li_to_move)
-        should_move_course = false;
+        li_to_update.style.border = "1px solid red";
+    } else {
+        li_to_update.style.border = "";
     }
     return should_move_course;
 }
@@ -163,99 +166,111 @@ function drop(ev, course_element) {
     let should_move_course = true;
 
     if (course_num === "INTDSC 1003") {
-        dropFailedAlert("INTDSC 1003 must be taken in the first semester!", li_to_move);
-        should_move_course = false;
-    } else if (course_num === "CMP SCI 1000" && semester_num > 1) {
-        dropFailedAlert("CMP SCI 1000 must be taken in the first or second semester!", li_to_move);
-        should_move_course = false;
-    }
+        if (semester_num === 0) {
+            li_to_move.style.border = "";
+        } else {
+            li_to_move.style.border = "1px solid red";
+        }
+        // dropFailedAlert("INTDSC 1003 must be taken in the first semester!", li_to_move);
+        // should_move_course = false;
+    } else if (course_num === "CMP SCI 1000") {
+        if (semester_num > 1) {
+            li_to_move.style.border = "";
+        } else {
+            li_to_move.style.border = "1px solid red";
+        }
+        // dropFailedAlert("CMP SCI 1000 must be taken in the first or second semester!", li_to_move);
+        // should_move_course = false;
+    } else {
+        // check if the course is an elective
+        const elective = course_name === '[User Selects]';
 
-    // check if the course is an elective
-    const elective = course_name === '[User Selects]';
+        if (!elective && should_move_course) {
+            var items = selected_drop_ul.getElementsByTagName("li");
+            let course_info = null;
 
-    if (!elective && should_move_course) {
-        var items = selected_drop_ul.getElementsByTagName("li");
-        let course_info = null;
-
-        for (var i = 0; i < items.length; ++i) {
-            // Check if course is being dropped back into the same semester it was previously in
-            if (course_num == items[i].getAttribute("courseNum")) { 
-                li_to_move.removeAttribute("id");
-                return; // Stop drop() function since the list item is not being dropped in a new semester
+            for (var i = 0; i < items.length; ++i) {
+                // Check if course is being dropped back into the same semester it was previously in
+                if (course_num == items[i].getAttribute("courseNum")) { 
+                    li_to_move.removeAttribute("id");
+                    return; // Stop drop() function since the list item is not being dropped in a new semester
+                }
             }
-        }
 
-        const required_courses_dict_list = JSON.parse(document.getElementById("required_courses_dict_list_unchanged").value);
+            const required_courses_dict_list = JSON.parse(document.getElementById("required_courses_dict_list_unchanged").value);
 
-        required_courses_dict_list.some((course_array) => {
-            if (course_array[0] === course_num) {
-                course_info = course_array[1];
-                return true;
+            required_courses_dict_list.some((course_array) => {
+                if (course_array[0] === course_num) {
+                    course_info = course_array[1];
+                    return true;
+                }
+            });
+
+            if (!course_info.semesters_offered.includes(course_schedule[semester_num].semester)) {
+                // dropFailedAlert(`${course_num} is not offered during the ${course_schedule[semester_num].semester} semester!`, li_to_move)
+                // should_move_course = false;
+                li_to_move.style.border = "1px solid red";
+            } else {
+                li_to_move.style.border = "";
             }
-        });
 
-        if (!course_info.semesters_offered.includes(course_schedule[semester_num].semester)) {
-            dropFailedAlert(`${course_num} is not offered during the ${course_schedule[semester_num].semester} semester!`, li_to_move)
-            should_move_course = false;
-        }
+            if ((course_info["prerequisite"].length != 0) && should_move_course) {
+                prereqVerification(course_info, course_num, semester_num, li_to_move, course_name)
+            }
 
-        if ((course_info["prerequisite"].length != 0) && should_move_course) {
-            should_move_course = prereqVerification(course_info, course_num, semester_num, li_to_move, course_name)
-        }
+            course_prereqs_for = JSON.parse(document.getElementById("course_prereqs_for").value);
+            course_prereqs_for_selected_course = course_prereqs_for[course_num]
 
-        course_prereqs_for = JSON.parse(document.getElementById("course_prereqs_for").value);
-        course_prereqs_for_selected_course = course_prereqs_for[course_num]
-
-        if (course_prereqs_for_selected_course) {
-            should_move_course = course_prereqs_for_selected_course.some((prereq) => {
-                prereq_for_course_info = null;
-                required_courses_dict_list.some((course_array) => {
-                    if (course_array[0] === prereq) {
-                        prereq_for_course_info = course_array[1];
-                        return true;
-                    }
-                });
-                return prereqVerification(prereq_for_course_info, prereq, semester_num, li_to_move, null, true, course_num)
-            })
-        }
+            if (course_prereqs_for_selected_course) {
+                course_prereqs_for_selected_course.forEach((prereq) => {
+                    prereq_for_course_info = null;
+                    required_courses_dict_list.some((course_array) => {
+                        if (course_array[0] === prereq) {
+                            prereq_for_course_info = course_array[1];
+                            return true;
+                        }
+                    });
+                    prereqVerification(prereq_for_course_info, prereq, semester_num, li_to_move, null, true, course_num)
+                })
+            }
+        }        
     }
 
-    if (should_move_course) {
-        var li_to_move_original_parent = li_to_move.parentNode;
-        var li_to_move_original_semester_num = parseInt(li_to_move_original_parent.parentNode.getAttribute("semesterNum"));
-        var li_to_move_original_semester_credits_element = document.getElementById(`semester-${li_to_move_original_semester_num}-credits`);
-        var li_to_move_original_semester_credits = parseInt(li_to_move_original_semester_credits_element.childNodes[1].textContent);
-        var original_semester_updated_credits = li_to_move_original_semester_credits - course_credits;
 
-        var new_li_parent_semester_num = parseInt(selected_drop_ul.parentNode.getAttribute("semesterNum"));
-        var new_li_parent_semester_credits_element = document.getElementById(`semester-${new_li_parent_semester_num}-credits`);
-        var new_li_parent_semester_credits = parseInt(new_li_parent_semester_credits_element.childNodes[1].textContent);
-        var new_semester_updated_credits = new_li_parent_semester_credits + course_credits;
+    var li_to_move_original_parent = li_to_move.parentNode;
+    var li_to_move_original_semester_num = parseInt(li_to_move_original_parent.parentNode.getAttribute("semesterNum"));
+    var li_to_move_original_semester_credits_element = document.getElementById(`semester-${li_to_move_original_semester_num}-credits`);
+    var li_to_move_original_semester_credits = parseInt(li_to_move_original_semester_credits_element.childNodes[1].textContent);
+    var original_semester_updated_credits = li_to_move_original_semester_credits - course_credits;
 
-        new_li_parent_semester_credits_element.childNodes[1].textContent = new_semester_updated_credits;
+    var new_li_parent_semester_num = parseInt(selected_drop_ul.parentNode.getAttribute("semesterNum"));
+    var new_li_parent_semester_credits_element = document.getElementById(`semester-${new_li_parent_semester_num}-credits`);
+    var new_li_parent_semester_credits = parseInt(new_li_parent_semester_credits_element.childNodes[1].textContent);
+    var new_semester_updated_credits = new_li_parent_semester_credits + course_credits;
 
-        li_to_move_original_semester_credits_element.childNodes[1].textContent = original_semester_updated_credits;
-        
-        var added_course = {
-            course: course_num,
-            credits: course_credits,
-            description: course_desc,
-            name: course_name
-        };
+    new_li_parent_semester_credits_element.childNodes[1].textContent = new_semester_updated_credits;
 
-        course_schedule[new_li_parent_semester_num].schedule.push(added_course);
-        course_schedule[new_li_parent_semester_num].credits = new_semester_updated_credits;
+    li_to_move_original_semester_credits_element.childNodes[1].textContent = original_semester_updated_credits;
+    
+    var added_course = {
+        course: course_num,
+        credits: course_credits,
+        description: course_desc,
+        name: course_name
+    };
 
-        var index_of_old_course_location = course_schedule[li_to_move_original_semester_num].schedule.findIndex(i => i.course === course_num);
+    course_schedule[new_li_parent_semester_num].schedule.push(added_course);
+    course_schedule[new_li_parent_semester_num].credits = new_semester_updated_credits;
 
-        if (index_of_old_course_location > -1) { // only splice array when item is found
-            course_schedule[li_to_move_original_semester_num].schedule.splice(index_of_old_course_location, 1); // 2nd parameter means remove one item only
-            course_schedule[li_to_move_original_semester_num].credits = original_semester_updated_credits;
-        }
+    var index_of_old_course_location = course_schedule[li_to_move_original_semester_num].schedule.findIndex(i => i.course === course_num);
 
-        document.getElementById("course_schedule").value = JSON.stringify(course_schedule);
-
-        selected_drop_ul.appendChild(li_to_move);
+    if (index_of_old_course_location > -1) { // only splice array when item is found
+        course_schedule[li_to_move_original_semester_num].schedule.splice(index_of_old_course_location, 1); // 2nd parameter means remove one item only
+        course_schedule[li_to_move_original_semester_num].credits = original_semester_updated_credits;
     }
-    li_to_move.removeAttribute("id");
+
+    document.getElementById("course_schedule").value = JSON.stringify(course_schedule);
+
+    li_to_move.setAttribute("id", course_num);
+    selected_drop_ul.appendChild(li_to_move);
 }
