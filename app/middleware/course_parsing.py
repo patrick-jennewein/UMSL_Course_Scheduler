8b010da351b0c8c66255ec5c0170d16511cb8c84
Jordan Moore <jmoore@marathonts.com>
2024-06-21 15:06:55 -0500

    Allow a user to select multiple certificates. Still a WIP in some regards, but functionally works

--- app/middleware/course_parsing.py
+++ app/middleware/course_parsing.py
@@ -127,10 +127,18 @@ def build_dictionary(courses: Union[dict, list]) -> dict:
             for term in course['rotation_term']:
                 course["semesters_offered"].append(term['term'])
         else:
-            course["semesters_offered"] = course['rotation_term']['term']
+            course["semesters_offered"].append(course['rotation_term']['term'])
 
         if 'prerequisite_description' in course:
             course['prerequisite_description'] = course['prerequisite_description']
+
+        course["required_by_major_cert"] = []
+        if ('required' in course) and ('major_or_cert' in course['required']):
+            if isinstance(course['required']['major_or_cert'], list):
+                course["required_by_major_cert"] = course['required']['major_or_cert']
+            else:
+                course["required_by_major_cert"].append(course['required']['major_or_cert'])
+
         # make final update to course dictionary
         updated_course_dict.update(course_dict)
 
@@ -546,17 +554,17 @@ def generate_semester(request): # -> dict[Union[str, Any], Union[Union[str, list
             courses_taken = list(dict.fromkeys(courses_taken))
 
         # if user elects to complete a certificate, get course data for that certificate and decrease electives accordingly
-        certificate_choice = request.form["certificate_choice"].split(",")
-        certificate_choice_name = certificate_choice[0]
-        certificate_choice_xml_tag = certificate_choice[1]
+        # certificate_choice = request.form["certificate_choice"].split(",")
+        # certificate_choice_name = certificate_choice[0]
+        # certificate_choice_xml_tag = certificate_choice[1]
 
-        if (certificate_choice_xml_tag != ""):
-            certificate_core, certificate_electives, cert_elective_courses_still_needed = parse_certificate(certificate_choice_xml_tag)
+        # if (certificate_choice_xml_tag != ""):
+        #     certificate_core, certificate_electives, cert_elective_courses_still_needed = parse_certificate(certificate_choice_xml_tag)
 
-            # Update counters according to certificate addition
-            min_3000_course_still_needed -= cert_elective_courses_still_needed
-            TOTAL_CREDITS_FOR_CERTIFICATE_ELECTIVES = cert_elective_courses_still_needed * DEFAULT_CREDIT_HOURS
-            #certificate_option = True
+        #     # Update counters according to certificate addition
+        #     min_3000_course_still_needed -= cert_elective_courses_still_needed
+        #     TOTAL_CREDITS_FOR_CERTIFICATE_ELECTIVES = cert_elective_courses_still_needed * DEFAULT_CREDIT_HOURS
+        #     #certificate_option = True
 
         # determine the semesters that user will be enrolled in
         user_semesters = build_semester_list(current_semester, include_summer)
@@ -564,15 +572,28 @@ def generate_semester(request): # -> dict[Union[str, Any], Union[Union[str, list
         # generate required courses
         all_courses_dict = json.loads(request.form['required_courses_dict'])
 
-        # if a certificate was selected, add the required certificate courses to required courses and update counters
-        if certificate_core:
-            num_courses_in_base_csdeg = len(all_courses_dict)
-            all_courses_dict.update(certificate_core)
-            num_3000_replaced_by_cert_core = len(all_courses_dict) - num_courses_in_base_csdeg
-            # print(f"{'3000+ Electives Used in Certificate':<40}{num_3000_replaced_by_cert_core}")
+        certs_selected = json.loads(request.form["selected_certificates"])
 
-            # update counters according to certificate selection
-            min_3000_course_still_needed -= num_3000_replaced_by_cert_core
+        certificate_choice = ""
+        # certificate_choice_name = certificate_choice[0]
+        # certificate_choice_xml_tag = certificate_choice[1]
+        cert_xml_tag_list = []
+
+        for cert in certs_selected:
+            certificate_choice = cert.split(",")
+            certificate_choice_name = certificate_choice[0]
+            certificate_choice_xml_tag = certificate_choice[1]
+            cert_xml_tag_list.append(certificate_choice_xml_tag)
+        
+        # if a certificate was selected, add the required certificate courses to required courses and update counters
+        # if (len(certs_selected)):
+        #     num_courses_in_base_csdeg = len(all_courses_dict)
+        #     all_courses_dict.update(certificate_core)
+        #     num_3000_replaced_by_cert_core = len(all_courses_dict) - num_courses_in_base_csdeg
+        #     # print(f"{'3000+ Electives Used in Certificate':<40}{num_3000_replaced_by_cert_core}")
+
+        #     # update counters according to certificate selection
+        #     min_3000_course_still_needed -= num_3000_replaced_by_cert_core
 
         ############################################################################
         ### Note to self: after 'if' statements of course rules, loop through list to build course dictionary
@@ -580,16 +601,11 @@ def generate_semester(request): # -> dict[Union[str, Any], Union[Union[str, list
 
         #courses_for_graduation = sorted(({k:v for (k,v) in all_courses_dict.items() if "required" in v.keys() and v['required'] == 'true'}).keys())
 
-        courses_for_graduation = []
-        for k, v in all_courses_dict.items():
-            if "required" in v:
-                major_or_cert = v['required']['major_or_cert']
-                if not isinstance(major_or_cert, list):
-                    major_or_cert = [major_or_cert]
-                for item in major_or_cert:
-                    if item == "BSComputerScience":
-                        print(f"{k}: {item}")
-                        courses_for_graduation.append(k)
+        courses_for_graduation = sorted(
+                ({k:v for (k,v) in all_courses_dict.items()
+                  if len(v['required_by_major_cert']) and
+                  ('BSComputerScience' in v['required_by_major_cert'] or len(set(cert_xml_tag_list).intersection(v['required_by_major_cert'])))}).keys()
+            )
 
         required_courses_tuple = tuple(copy.deepcopy(courses_for_graduation))
         print(f"{courses_for_graduation=}")
--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,13 @@ from pprint import pprint
 def index():
     # set up defaults
     semesters = ["Fall", "Spring"]
-    certificates = [("None", ""), ("Artificial Intelligence", "AICERTReq"), ("Cybersecurity", "CYBERCERTReq"), ("Data Science", "DATACERTReq"), ("Mobile Apps and Computing", "MOBILECERTReq"), ("Internet and Web", "WEBCERTReq")]
+    certificates = [
+        ("Artificial Intelligence", "AICERTReq"),
+        ("Cybersecurity", "CYBERCERTReq"),
+        ("Data Science", "DATACERTReq"),
+        ("Mobile Apps and Computing", "MOBILECERTReq"),
+        ("Internet and Web", "WEBCERTReq")
+    ]
 
     # create a list of all courses
     all_courses = parse_courses()
@@ -51,7 +57,8 @@ def index():
                            fe_taken = 0,
                            is_graduated = False,
                            required_courses_tuple = json.dumps([]),
-                           certificate_choice = json.dumps([])
+                           certificate_choice = json.dumps([]),
+                           selected_certificates = json.dumps([])
     )
 
 @app.route('/schedule', methods=["POST"])
