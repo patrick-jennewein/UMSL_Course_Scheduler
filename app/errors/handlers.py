dd790ee0fe2a5513cb8e3ada395525df2dc7f838
Patrick Jennewein <jenneweinp@gmail.com>
2024-07-03 17:41:27 -0500

    Actual error pages finished.

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -12,8 +12,8 @@ def error_403(error):
 
 @errors.app_errorhandler(405)
 def error_405(error):
-    return render_template('errors/404.html'), 405
+    return render_template('errors/405.html'), 405
 
 @errors.app_errorhandler(500)
 def error_500(error):
-    return render_template('errors/404.html'), 500
+    return render_template('errors/500.html'), 500
