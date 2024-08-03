ff9980f1d10fa48b13dbc6464bf8c0ff694354d6
Jordan Moore <jmoore@marathonts.com>
2024-08-03 07:50:52 -0500

    Attempt static_url_path usage

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-app = Flask(__name__, static_url_path='/UndergradMap/static')
+app = Flask(__name__, static_url_path='/UndergradMap/static', static_folder='app/static')
 
 from app import routes
 from app.errors.handlers import errors
