6c2c47e030eb34ccbf5d904f3dd401f97a0761ab
Jordan Moore <jmoore@marathonts.com>
2024-08-02 23:10:34 -0500

    Update static_url_path

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-app = Flask(__name__, static_url_path='', static_folder='app/static', template_folder='app/templates')
+app = Flask(__name__, static_url_path='/app/static')
 
 from app import routes
 from app.errors.handlers import errors
